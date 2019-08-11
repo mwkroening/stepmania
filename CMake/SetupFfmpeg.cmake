c5fb3d7392221d7444ec67c1aef3be598f12c3c1
Martin Kröning <m.kroening@hotmail.de>
2019-08-11 16:50:38 +0200

    Maybe both?

--- CMake/SetupFfmpeg.cmake
+++ CMake/SetupFfmpeg.cmake
@@ -47,10 +47,9 @@ externalproject_add(ffmpeg
   INSTALL_COMMAND   ""
 )
 
-#externalproject_get_property(ffmpeg SOURCE_DIR)
-#set(FFMPEG_INCLUDE_DIR ${SOURCE_DIR})
+externalproject_get_property(ffmpeg SOURCE_DIR)
 externalproject_get_property(ffmpeg BINARY_DIR)
-set(FFMPEG_INCLUDE_DIR ${BINARY_DIR})
+set(FFMPEG_INCLUDE_DIRS ${SOURCE_DIR} ${BINARY_DIR})
 set(FFMPEG_STATIC_LIBRARY_DIRS ${BINARY_DIR}/doc/examples/pc-uninstalled/../../../libavformat
                                ${BINARY_DIR}/doc/examples/pc-uninstalled/../../../libavcodec
                                ${BINARY_DIR}/doc/examples/pc-uninstalled/../../../libavutil
