d9d5e9d44f00bd3511880ac60d6131d711aae842
Martin Kröning <m.kroening@hotmail.de>
2019-08-13 12:27:42 +0200

    Another one

--- CMake/SetupFfmpeg.cmake
+++ CMake/SetupFfmpeg.cmake
@@ -47,11 +47,12 @@ externalproject_add(ffmpeg
 #  INSTALL_COMMAND   ""
 )
 
-#externalproject_get_property(ffmpeg SOURCE_DIR)
+externalproject_get_property(ffmpeg SOURCE_DIR)
 externalproject_get_property(ffmpeg BINARY_DIR)
 find_package(FFMPEG REQUIRED
   COMPONENTS        avcodec avformat avutil
-  PATH              ${BINARY_DIR})
+  PATH              ${BINARY_DIR}
+                    ${SOURCE_DIR})
 
 # BINARY_DIR includes libavutil/avconfig.h
 #set(FFMPEG_INCLUDE_DIRS ${SOURCE_DIR} ${BINARY_DIR})
