2b883dc43065137b5cb241e62cf322f0234a6930
Martin Kröning <m.kroening@hotmail.de>
2019-09-08 23:51:11 +0200

    Add IteratorBackInserterDemo

---
+++ iterator_back_inserter.cpp
@@ -0,0 +1,39 @@
+// iterator_back_inserter.cpp
+// compile with: /EHsc
+#include <iterator>
+#include <vector>
+#include <iostream>
+
+int main( )
+{
+   using namespace std;
+   int i;
+
+   vector<int> vec;
+   for ( i = 0 ; i < 3 ; ++i )
+   {
+      vec.push_back ( i );
+   }
+
+   vector <int>::iterator vIter;
+   cout << "The initial vector vec is: ( ";
+   for ( vIter = vec.begin ( ) ; vIter != vec.end ( ); vIter++)
+      cout << *vIter << " ";
+   cout << ")." << endl;
+
+   // Insertions can be done with template function
+   back_insert_iterator<vector<int> > backiter ( vec );
+*backiter = 30;
+   backiter++;
+*backiter = 40;
+
+   // Alternatively, insertions can be done with the
+   // back_insert_iterator member function
+   back_inserter ( vec ) = 500;
+   back_inserter ( vec ) = 600;
+
+   cout << "After the insertions, the vector vec is: ( ";
+   for ( vIter = vec.begin ( ) ; vIter != vec.end ( ); vIter++ )
+      cout << *vIter << " ";
+   cout << ")." << endl;
+}
